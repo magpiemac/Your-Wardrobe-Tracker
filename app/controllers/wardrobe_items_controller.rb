a5e25f675e555d136a2baa0a17a19658360fccf7
Shannon McCormack <shannonkmc@hotmail.com>
2017-10-03 16:55:44 +0000

    added authenticate user! to capsules, wardrobeitems and welcome controller

--- app/controllers/wardrobe_items_controller.rb
+++ app/controllers/wardrobe_items_controller.rb
@@ -1,4 +1,5 @@
 class WardrobeItemsController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_wardrobe_item, only: [:show, :edit, :update, :destroy]
 
   # GET /wardrobe_items
