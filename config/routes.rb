96c901fdb66a58764a57dc9e9ca6605d0106b619
Shannon McCormack <shannonkmc@hotmail.com>
2017-10-03 16:46:36 +0000

    added _nav partial, updated routes for authenicated user

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   root 'welcome#home'
-  resources :wardrobe_item_capsules
   devise_for :users
+
+  authenticated :user do
+    root to: 'wardrobe_items#index', as: :authenticated_root
+  end
+  
+  resources :wardrobe_item_capsules
   resources :wardrobe_items
   resources :capsules
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
