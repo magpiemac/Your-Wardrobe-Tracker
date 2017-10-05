578307759075cc3bbd537f41f42fd18c79996d7b
Shannon McCormack <shannonkmc@hotmail.com>
2017-10-05 15:14:54 +0000

    removed excess notice from wardrobeitems index view, tweaked routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,12 @@ Rails.application.routes.draw do
 
   root 'welcome#home'
 
-  resources :wardrobe_item_capsules
-  resources :wardrobe_items
-  resources :capsules
+  resources :wardrobe_items do
+    resources :capsules, only: [:index, :create, :new, :edit, :show]
+  end
+
+  resources :capsules do
+    resources :wardrobe_items, only: [:index, :create, :new, :edit, :show]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
