a68a2a9677284b07a9f90477284390c934f0c720
Shannon McCormack <shannonkmc@hotmail.com>
2017-10-19 01:17:17 +0000

    added nested new link to capsules/show and contoller

--- app/models/capsule.rb
+++ app/models/capsule.rb
@@ -2,6 +2,7 @@ class Capsule < ApplicationRecord
   belongs_to :user
   has_many :wardrobe_item_capsules
   has_many :wardrobe_items, through: :wardrobe_item_capsules
+  accepts_nested_attributes_for :wardrobe_items
 
   validates :name, presence: true, uniqueness: true
 
