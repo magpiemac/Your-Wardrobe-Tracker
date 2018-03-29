2b6c7c87511335bc2c8ff2176b72c64cc75d18ae
github username <github email address>
2018-03-29 15:12:26 -0400

    Removed coffee text from js files. Added clickhandlers in wardrobe_items.js.

--- app/assets/javascripts/wardrobe_items.js
+++ app/assets/javascripts/wardrobe_items.js
@@ -1,3 +1,10 @@
-# Place all the behaviors and hooks related to the matching controller here.
-# All this logic will automatically be available in application.js.
-# You can use CoffeeScript in this file: http://coffeescript.org/
+$(() => {
+  bindClickHandlers()
+})
+
+const bindClickHandlers = () => {
+  $('.wardrobe_items_path').on('click', (e) => {
+    e.preventDefault()
+    console.log('hello')
+  })
+}
