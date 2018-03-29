61f68d6ad15b155952d4830faadd022b188f3e0b
github username <github email address>
2018-03-29 18:11:02 -0400

    Troublshooting fetch error.

---
+++ app/assets/javascripts/wardrobe_items.js
@@ -0,0 +1,13 @@
+$(() => {
+  bindClickHandlers()
+})
+
+const bindClickHandlers = () => {
+  $('.all_wardrobe_items').on('click', (e) =>{
+    e.preventDefault()
+    //console.log('hello')
+    // fetch('/wardrobe_items.json')
+    //   .then(foo => foo.json())
+    //   .then(bar => console.log(bar))
+  })
+}
