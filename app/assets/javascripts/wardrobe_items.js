edc175d65aeaaf28169d53eda959261843ae9f2f
github username <github email address>
2018-03-30 11:27:32 -0400

    Fixed div id in application.html.erb, added and tested a Wardrobe_item constructor function.

--- app/assets/javascripts/wardrobe_items.js
+++ app/assets/javascripts/wardrobe_items.js
@@ -9,7 +9,18 @@ const bindClickHandlers = () => {
       credentials: "include"})
       .then(res => res.json())
       .then(wardrobe_items => {
-        $('#app-container').html('hello')
+        $('#app-container').html(' ')
+        wardrobe_items.forEach(wardrobe_item => {
+          let newWardrobe_item = new Wardrobe_item(wardrobe_item)
+          console.log(newWardrobe_item)
       })
+    })
   })
+}
+
+function Wardrobe_item(wardrobe_item) {
+  this.capsules = wardrobe_item.capsules
+  this.description = wardrobe_item.description
+  this.id = wardrobe_item.id
+  this.item = wardrobe_item.item
 }
