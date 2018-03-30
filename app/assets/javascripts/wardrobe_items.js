fec3bb0fff4c3c136e89d0b05a57d788ecf4ba3f
github username <github email address>
2018-03-30 11:55:52 -0400

    Added link to prototype.

--- app/assets/javascripts/wardrobe_items.js
+++ app/assets/javascripts/wardrobe_items.js
@@ -1,3 +1,5 @@
+
+//document.ready
 $(() => {
   bindClickHandlers()
 })
@@ -5,6 +7,7 @@ $(() => {
 const bindClickHandlers = () => {
   $('.all_wardrobe_items').on('click', (e) => {
     e.preventDefault()
+    history.pushState(null, null, "wardrobe_item")
     fetch('/wardrobe_items.json', {
       credentials: "include"})
       .then(res => res.json())
@@ -12,11 +15,14 @@ const bindClickHandlers = () => {
         $('#app-container').html(' ')
         wardrobe_items.forEach(wardrobe_item => {
           let newWardrobe_item = new Wardrobe_item(wardrobe_item)
-          console.log(newWardrobe_item)
+          let wardrobe_itemHtml = newWardrobe_item.formatIndex()
+          $('#app-container').append(wardrobe_itemHtml)
       })
     })
   })
 }
+
+//constructor function
 
 function Wardrobe_item(wardrobe_item) {
   this.capsules = wardrobe_item.capsules
@@ -24,3 +30,13 @@ function Wardrobe_item(wardrobe_item) {
   this.id = wardrobe_item.id
   this.item = wardrobe_item.item
 }
+
+//every instance of Wardrobe_item can call the .formatIndex function.
+//cannot use arrow function with prototype
+//format the rest of html
+Wardrobe_item.prototype.formatIndex = function() {
+  let wardrobe_itemHtml = `
+    <a href="/wardrobe_items/${this.id}" class="show_link"<h1>${this.item}</h1></a>
+  `
+    return wardrobe_itemHtml
+};
